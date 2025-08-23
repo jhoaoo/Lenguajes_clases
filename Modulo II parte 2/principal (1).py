c3e990cda817a339a3ddfea209130affa965e564
Jhoaoo Sebastian Llerena Quispe <164796968+jhoaoo@users.noreply.github.com>
2025-08-22 22:52:19 -0500

    Delete Modulo II parte 2 directory

--- Modulo II parte 2/principal (1).py
+++
@@ -1,9 +0,0 @@
-from validaciones import validar_contraseña
-
-contraseña = input("Ingrese una contraseña para validar: ")
-resultado = validar_contraseña(contraseña)
-
-if resultado is True:
-    print("Contraseña válida")
-else:
-    print(resultado)
--- Modulo II parte 2/principal.py
+++
@@ -1,9 +0,0 @@
-from validaciones import validar_fecha
-
-fecha = input("Ingrese una fecha en formato dd/mm/yyyy: ")
-resultado = validar_fecha(fecha)
-
-if resultado is True:
-    print("Fecha válida")
-else:
-    print(resultado)
